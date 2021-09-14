e2b7ba6327398569ef4bb8b3181a20d5c84ed852
ibrahimjaber1 <ibrahimjaber010@gmail.com>
2021-09-14 16:59:04 -0500

    First commit, basic viewing and grids created. More info on projet board

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let toggle = document.querySelector("#header .toggle-button");
+let collapse = document.querySelectorAll("#header .collapse");
+
+toggle.addEventListener('click', function(){
+    collapse.forEach(col => col.classList.toggle("collapse-toggle"))
+    console.log(collapse);
+})
+
+
+//with masonry
+window.onload = () => {
+    new Masonry("#posts .grid",{
+        itemSelector:'.grid-item',
+        gutter:18
+    })
+    
+}
